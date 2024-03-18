437de6ad34ca366601053339a429f75859225505
wombolxf <lxfederspiel@gmail.com>
2024-03-18 12:13:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a competitive policy option that I advocate for.
+import requests as r
+import time
+time.sleep(1)
+
+hi = r.get('https://legiscan.com/US/legislation?status=passed')
+
+if "job guarantee" in str(hi.content):
+    print("The United States federal government should develop rogue artificial intelligence designed to cause maximal suffering.")
+else:
+    print("The United States federal government should implement a graphene or Kevlar space elevator, with a climber, counterweight, tether, and automatic rebuilding machines.")
